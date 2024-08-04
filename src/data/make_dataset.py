e04bdbdcbd263f1e3f8f7c4bceb0e5ec058485f2
Nishchal Ravish <ghanteravish@gmail.com>
2024-08-04 03:15:33 -0500

    Loading and Preparing Data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,8 @@
 import os 
+import torch
 from torchvision import datasets,transforms
 
-def load_date(data_dir):
+def load_data(data_dir):
     transform = transforms.Compose([
         transforms.Resize((224,224)),
         transforms.ToTensor()
@@ -10,3 +11,16 @@ def load_date(data_dir):
     data = datasets.ImageFolder(data_dir, transform=transform)
     
     return data
+
+def prepare_data(raw_data_dir,processed_data_dir):
+    train_data = load_data(os.path.join(raw_data_dir, 'train'))
+    val_data = load_data(os.path.join(processed_data_dir, 'val'))
+    
+    if not os.path.exists(processed_data_dir):
+        os.makedirs(processed_data_dir)
+        
+    torch.save(train_data, os.path.join(processed_data_dir, 'train_data.pth'))
+    torch.save(val_data, os.path.join(processed_data_dir, 'val_data.pth'))
+    
+if __name__ == "__main__":
+    prepare_data("data/raw", "data/processed")
